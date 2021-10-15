034f270aa3cbc8015646af46994c9d155a597194
pierre <pnavarin@gmail.com>
2021-10-15 15:53:36 -0400

    Finished clock functionality

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -3,14 +3,36 @@ import React from 'react';
 class Clock extends React.Component {
     constructor(props) {
         super(props)
+        
+        this.state = {
+            date: new Date(),
+            // dateString: this.date.toDateString(),
+            // hours: this.date.getHours(),
+            // minutes: this.date.getMinutes(),
+            // seconds: this.date.getSeconds()
+        }
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount(){
+        setInterval(this.tick, 1000);
+    }
+
+    tick(){
+        this.setState({date: new Date()});
     }
 
     render() {
-        console.log('in Clock render')
+        let date = this.state.date.toDateString()
+        let hours = this.state.date.getHours()
+        let minutes = this.state.date.getMinutes()
+        let seconds = this.state.date.getSeconds()
 
         return (
             <div>
                 <h1>GP Clock</h1>
+                <h3>{date}</h3>
+                <h3>{hours}:{minutes}:{seconds}</h3>
             </div> 
         )
     }
